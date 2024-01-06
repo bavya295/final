4812f89461bbff12c2369e5f98b983fe7c3e49cf
Bavya <bavya295@gmail.com>
2024-01-06 14:44:44 +0530

    Updated code

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,10 @@ import { Link } from "react-router-dom"
 export default function Home(){
     return(
         <>
+         <h1>Happy birthday</h1>
         <Link to="/register">
             <button>Clcike me</button>
+           
         </Link>
         </>
     )
